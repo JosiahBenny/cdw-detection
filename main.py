c60e9071806b1281f83317407ba96c6db08d4f22
JosiahBenny <97804701+JosiahBenny@users.noreply.github.com>
2025-04-07 21:49:20 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from ultralytics import solutions
 
 inf = solutions.Inference(
-    model="C:/Users/USER/Documents/cdw/yolo models/yolov8s_100ep/detect/train\weights/best.pt",
+    model="best.pt",
 )
 inf.inference()
